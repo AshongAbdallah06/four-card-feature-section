af84335b89f4636f3d8ad7bb062a5764de4317cb
AshongAbdallah06 <ashongabdalla15@gmail.com>
2024-03-11 20:09:54 +0200

    Update files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import supervisor from './images/icon-supervisor.svg';
+import teamBuilder from './images/icon-team-builder.svg';
+import karma from './images/icon-karma.svg';
+import calculator from './images/icon-calculator.svg';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                <p className='header-text'>
+                    Reliable, efficient delivery
+                </p>
+                <h1>
+                    Powered by Technology
+                </h1>
+                <p className='text'>
+                    Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful
+                </p>
+            </header>
+
+            <main>
+                <div className='block border-top blue'>
+                    <div className="heading">Supervisor</div>
+                    <p className="block-text">
+                        Monitors activity to identify project roadblocks
+                    </p>
+                    
+                    <img src={supervisor} alt="" />
+                </div>
+
+                <div className='cont'>
+                    <div className='block border-top red'>
+                        <div className="heading">Team Builder</div>
+                        <p className="block-text">
+                            Scans our talent network to create the optimal team for your project
+                        </p>
+                        
+                        <img src={teamBuilder} alt="" />
+                    </div>
+
+                    <div className='block border-top orange'>
+                        <div className="heading">Karma</div>
+                        <p className="block-text">
+                            Regularly evaluates our talent to ensure quality
+                        </p>
+                        
+                        <img src={karma} alt="" />
+                    </div>
+                </div>
+
+                <div className='block border-top darkblue'>
+                    <div className="heading">Supervisor</div>
+                    <p className="block-text">
+                        Uses data from past projects to provide better delivery estimates
+                    </p>
+                    
+                    <img src={calculator} alt="" />
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
